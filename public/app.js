ae05b1a5bc3b948a05572984ea6784126f65ceab
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-06-24 20:38:47 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,31 +2,30 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const resetBtn = document.getElementById('resetBtn');
 const eraserBtn = document.getElementById('eraserBtn');
+const eraserSizeInput = document.getElementById('eraserSize');
 let drawing = false;
 let erasing = false;
+let eraserSize = eraserSizeInput.value;
 
-// Set up the canvas dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const socket = io();
 
-// Utility function to draw on the canvas
 function draw(x, y) {
     if (!drawing) return;
     if (erasing) {
         context.globalCompositeOperation = 'destination-out';
-        context.lineWidth = 10;
+        context.lineWidth = eraserSize;
     } else {
         context.globalCompositeOperation = 'source-over';
         context.lineWidth = 2;
     }
     context.lineTo(x, y);
     context.stroke();
-    socket.emit('drawing', { x, y, erasing });
+    socket.emit('drawing', { x, y, erasing, eraserSize });
 }
 
-// Event listeners for drawing
 canvas.addEventListener('mousedown', () => {
     drawing = true;
     context.beginPath();
@@ -42,11 +41,10 @@ canvas.addEventListener('mousemove', (event) => {
     }
 });
 
-// Handle receiving drawings from other users
 socket.on('drawing', (data) => {
     if (data.erasing) {
         context.globalCompositeOperation = 'destination-out';
-        context.lineWidth = 10;
+        context.lineWidth = data.eraserSize;
     } else {
         context.globalCompositeOperation = 'source-over';
         context.lineWidth = 2;
@@ -55,29 +53,28 @@ socket.on('drawing', (data) => {
     context.stroke();
 });
 
-// Clear the canvas
 function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-// Handle reset button click
 resetBtn.addEventListener('click', () => {
     clearCanvas();
     socket.emit('reset');
 });
 
-// Handle eraser button click
 eraserBtn.addEventListener('click', () => {
     erasing = !erasing;
     eraserBtn.textContent = erasing ? 'Drawing' : 'Eraser';
 });
 
-// Handle reset event from server
+eraserSizeInput.addEventListener('input', (event) => {
+    eraserSize = event.target.value;
+});
+
 socket.on('reset', () => {
     clearCanvas();
 });
 
-// Ensure the canvas resizes correctly with the window
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;

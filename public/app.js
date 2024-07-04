73ce18f4d1f57963229789b57b7da16ab2893eef
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-07-04 09:39:43 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
-const canvas = document.getElementById("canvas")
+const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth - 60;
 canvas.height = 400;
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 ctx.fillStyle = "white";
 ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    
+    
